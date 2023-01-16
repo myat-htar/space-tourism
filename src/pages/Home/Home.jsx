d477c492dac347bd70c7b75fe0c14ed9ac157630
MyatHtar <myathtar99@gmail.com>
2023-01-16 22:17:23 +0630

    design home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 function Home() {
   return (
     <main className="container">
-      <h3>so, you want to travel to</h3>
-      <h1>space</h1>
+      <div className="text-wrapper">
+        <h1 className="title">
+          so, you want to travel to <span>space</span>
+        </h1>
+        <p className="para">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <Link to="/destination" className="self-end">
+        <button className="btn">explore</button>
+      </Link>
     </main>
   );
 }
